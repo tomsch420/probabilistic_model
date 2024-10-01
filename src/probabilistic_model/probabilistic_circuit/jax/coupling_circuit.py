6d150bd5e95f5a2308aab712554a631bcd471267
Tom Schierenbeck <tom_sch@uni-bremen.de>
2024-10-01 11:13:26 +0200

    Coupling circuit seems to work? lol

--- src/probabilistic_model/probabilistic_circuit/jax/coupling_circuit.py
+++ src/probabilistic_model/probabilistic_circuit/jax/coupling_circuit.py
@@ -6,7 +6,7 @@ from jax.tree_util import tree_flatten, tree_unflatten
 import equinox as eqx
 
 from .probabilistic_circuit import Layer
-
+import jax.numpy as jnp
 
 class Conditioner:
 
@@ -24,16 +24,88 @@ class Conditioner:
 
 
 class CouplingCircuit(eqx.Module):
+    """
+    A probabilistic circuit that uses a function to generate parameters for a circuit.
+    """
 
     conditioner: Conditioner
-    circuit: Layer
+    """
+    The conditioner that generates the parameters for the circuit.
+    """
 
-    def __init__(self, conditioner: Conditioner, circuit: Layer):
+    circuit: Layer
+    """
+    The circuit to generate the parameters for.
+    """
+
+    conditioner_columns: jax.Array
+    """
+    The columns in a matrix that the conditioner takes as input for producing circuit parameters.
+    """
+
+    circuit_columns: jax.Array
+    """
+    The columns in a matrix that the circuit takes as input for calculating likelihoods.
+    """
+
+    def __init__(self, conditioner: Conditioner, conditioner_columns: jax.Array,
+                 circuit: Layer, circuit_columns):
         self.conditioner = conditioner
+        self.conditioner_columns = conditioner_columns
         self.circuit = circuit
+        self.circuit_columns = circuit_columns
 
     def partition_circuit(self):
+        """
+        Partition the circuit into the parameters and the static structure.
+        :return:
+        """
         return eqx.partition(self.circuit, eqx.is_inexact_array)
+
+    def create_circuit_from_parameters(self, params) -> Layer:
+        """
+        Generate a circuit with the structure from self.circuit and the parameters from params.
+        :param params: The parameters to be used in the circuit.
+        :return: The circuit
+        """
+
+        # get the parameters and circuit definition
+        tree_def, static = self.partition_circuit()
+
+        # flatten the parameters
+        flat_model, flat_tree_def = jax.tree_util.tree_flatten(tree_def)
+
+        # update the parameters
+        params = tree_unflatten(flat_tree_def, params)
+
+        # assemble the parameterized model
+        circuit = eqx.combine(params, static)
+        return circuit
+
+    def _conditional_log_likelihood_single(self, x, conditioner, create_circuit_from_parameters):
+        """
+        Calculate the conditional log likelihood of a single data point.
+
+        :param x: The datapoint
+        :param conditioner: The conditioner to use
+        :param create_circuit_from_parameters: The function to create a circuit from parameters
+        :return:
+        """
+        params = conditioner.generate_parameters(x[self.conditioner_columns]).reshape(1, -1)
+        circuit = create_circuit_from_parameters(params)
+        return circuit.log_likelihood_of_nodes(x[self.circuit_columns])
+
+    def vectorized_conditional_log_likelihood_single(self, x):
+        """
+        Calculate the conditional log likelihood of data points as batched.
+        :param x: The data points
+        :return: The conditional log likelihoods
+        """
+        return (jax.vmap(self._conditional_log_likelihood_single, in_axes=(0, None, None))
+                (x, self.conditioner, self.create_circuit_from_parameters))
+
+    def conditional_log_likelihood(self, x):
+        return self.vectorized_conditional_log_likelihood_single(x)
 
     def validate(self):
         self.circuit.validate()
@@ -41,11 +113,3 @@ class CouplingCircuit(eqx.Module):
         flattened_params = tree_flatten(params)[0]
         number_of_parameters = sum([len(p) for p in flattened_params])
         assert number_of_parameters == self.conditioner.output_length
-
-    def conditional_log_likelihood(self, x):
-        tree_def, static = self.partition_circuit()
-        flat_model, treedef_model = jax.tree_util.tree_flatten(tree_def)
-        params = self.conditioner.generate_parameters(x)
-        params = tree_unflatten(treedef_model, [params[0]])
-        circuit = eqx.combine(params, static)
-        return circuit.log_likelihood_of_nodes(x)
--- test/test_jax/test_coupling_circuit.py
+++ test/test_jax/test_coupling_circuit.py
@@ -1,4 +1,6 @@
 import unittest
+
+import jax
 import numpy as np
 
 from probabilistic_model.probabilistic_circuit.jax import UniformLayer, SumLayer
@@ -10,29 +12,51 @@ from jax.experimental.sparse import BCOO
 
 class TrivialConditioner(Conditioner):
 
-        def generate_parameters(self, x):
-            return jnp.log(jnp.array([[0.2, 0.8]]).repeat(x.shape[0], 0))
+    def generate_parameters(self, x):
+         return jnp.log(jnp.array([[0.2, 0.8]]).repeat(x.shape[0], 0))
 
-        @property
-        def output_length(self):
-            return 2
+    @property
+    def output_length(self):
+        return 2
+
+class LessTrivialConditioner(Conditioner):
+
+    linear = eqx.nn.Linear(1, 2, key=jax.random.PRNGKey(0))
+
+    def generate_parameters(self, x):
+        return self.linear(x)
+
+    @property
+    def output_length(self):
+        return self.linear.out_features
 
 class CouplingCircuitTestCase(unittest.TestCase):
 
-    data = np.vstack((np.random.uniform(0, 1, (100, 1)),
-                      np.random.uniform(2, 3, (200, 1))))
+    data = jnp.array(np.vstack((np.random.uniform(0, 1, (100, 1)),
+                      np.random.uniform(2, 3, (200, 1)))))
     uniform_layer = UniformLayer(0, jnp.array([[-0.01, 1.01],
                                                        [1.99, 3.01]]))
     sum_layer = SumLayer([uniform_layer], [BCOO((jnp.array([0., 0.]),
                                                  jnp.array([[0, 0], [0, 1]])),
                                                 shape=(1, 2))])
 
-    cc = CouplingCircuit(TrivialConditioner(), sum_layer)
-    cc.validate()
+    cc: CouplingCircuit
+
+    def setUp(self):
+        self.cc = CouplingCircuit(TrivialConditioner(), jnp.array([0]), self.sum_layer, jnp.array([0]))
+        self.cc.validate()
 
     def test_log_likelihood(self):
-        x = jnp.array(self.data)
-        ll = self.cc.conditional_log_likelihood(x)
+        ll = self.cc.conditional_log_likelihood(self.data)
+        ll1 = ll[0, 0]
+        ll2 = ll[-1, 0]
+        self.assertAlmostEqual(ll1, jnp.log(jnp.array([0.2/1.02])), delta=10e-4)
+        self.assertAlmostEqual(ll2, jnp.log(jnp.array([0.8/1.02])), delta=10e-4)
+
+    def test_vmap(self):
+        cc = CouplingCircuit(LessTrivialConditioner(), jnp.array([0]), self.sum_layer, jnp.array([0]))
+        r = cc.conditional_log_likelihood(self.data)
+        print(r.shape)
 
 if __name__ == '__main__':
     unittest.main()
