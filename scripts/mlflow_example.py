c3a5cd0f5558ca8376873e9779020219fd8a59e8
Tom Schierenbeck <tom_sch@uni-bremen.de>
2024-10-30 13:24:23 +0100

    Added mlflow integration support

---
+++ scripts/mlflow_example.py
@@ -0,0 +1,58 @@
+import json
+import tempfile
+
+import mlflow
+import numpy as np
+import plotly.graph_objects as go
+import tqdm
+from random_events.variable import Continuous
+
+from probabilistic_model.learning.nyga_distribution import NygaDistribution
+from probabilistic_model.mlflow_integration import infer_signature, ProbabilisticModelWrapper
+
+np.random.seed(69)
+
+mlflow.set_tracking_uri(uri=" http://127.0.0.1:8080")
+
+# training
+number_of_samples_per_component = 1000
+number_of_samples_for_evaluation = 5000
+number_of_components = 5
+min_samples_per_quantile = 60
+
+data = []
+for component in tqdm.trange(number_of_components, desc="Generating data"):
+    samples = np.random.normal(component, 1., (number_of_samples_per_component, 1))
+    data.append(samples)
+
+data = np.concatenate(data, axis=0)
+variable = Continuous("x")
+
+nx_model = NygaDistribution(variable, min_samples_per_quantile=min_samples_per_quantile)
+
+run = mlflow.start_run(run_name="Integration example with mlflow")
+
+# Log the hyperparameters
+mlflow.log_params({"min_samples_per_quantile": nx_model.min_samples_per_quantile})
+nx_model.fit(data)
+mlflow.set_tag("Training Info", "Basic LR model for iris data")
+
+file = tempfile.NamedTemporaryFile()
+
+with open(file.name, "w") as f:
+    json.dump(nx_model.to_json(), f)
+
+# Log the model
+model_info = mlflow.pyfunc.log_model(
+    artifact_path="mlflow_integration_test",
+    artifacts={"model_path": file.name},
+    signature=infer_signature(nx_model),
+    python_model=ProbabilisticModelWrapper(nx_model),
+    registered_model_name="tracking-quickstart",
+)
+
+loaded_model = mlflow.pyfunc.load_model(model_uri=run.info.artifact_uri + "/mlflow_integration_test")
+loaded_model = loaded_model.unwrap_python_model().model
+
+fig = go.Figure(loaded_model.plot(), loaded_model.plotly_layout())
+fig.show()
---
+++ src/probabilistic_model/mlflow_integration.py
@@ -0,0 +1,52 @@
+import mlflow.pyfunc
+from mlflow.models.signature import ModelSignature
+from mlflow.types import ColSpec, DataType, Schema
+from random_events.utils import SubclassJSONSerializer
+from random_events.variable import Continuous, Symbolic, Integer
+from typing_extensions import Optional
+import json
+
+from .probabilistic_model import ProbabilisticModel
+
+
+class ProbabilisticModelWrapper(mlflow.pyfunc.PythonModel):
+    """
+    Wrapper for a probabilistic model to be used with MLflow.
+
+    The wrapper requires the artifact "model_path" to contain a path to a file that can be parsed using
+    the SubclassJSONSerializer.
+    """
+
+    model: Optional[ProbabilisticModel] = None
+
+    def __init__(self, model: Optional[ProbabilisticModel] = None):
+        self.model = model
+        super().__init__()
+
+    def load_context(self, context):
+        with open(context.artifacts["model_path"], "r") as f:
+            model_dict = json.load(f)
+        self.model = SubclassJSONSerializer.from_json(model_dict)
+
+    def predict(self, context, model_input):
+        return self.model.log_likelihood(model_input)
+
+
+def infer_signature(model: ProbabilisticModel) -> ModelSignature:
+    """
+    Infer the signature of a probabilistic model.
+    :param model: The model to infer the signature from.
+    :return: The inferred signature.
+    """
+    inputs = []
+    for variable in model.variables:
+        if isinstance(variable, Continuous):
+            inputs.append(ColSpec(type=DataType.float, name=variable.name, required=False))
+        elif isinstance(variable, Symbolic):
+            inputs.append(ColSpec(type=DataType.string, name=variable.name, required=False))
+        elif isinstance(variable, Integer):
+            inputs.append(ColSpec(type=DataType.integer, name=variable.name, required=False))
+        else:
+            raise ValueError(f"Unknown variable type {type(variable)}")
+    result = ModelSignature(inputs=Schema(inputs))
+    return result
